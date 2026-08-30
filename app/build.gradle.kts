cbab17c4da8961fa64f026d523c9b1876458fccf
Yang136 <yang2007070206@163.com>
2026-08-30 20:40:11 +0800

    release: 1.8.0（网络诊断）

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.yang136.sshhelper"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10
-        versionName = "1.7.0"
+        versionCode = 11
+        versionName = "1.8.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
     }
